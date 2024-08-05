a9819fa89b96aeaf0f0e9dae68cb9fdfa4910933
Brahn71 <MRaudales71@gmail.com>
2024-08-05 13:15:30 -0600

    muestra de peso en web

--- lib/pantallas/logicaboton/tomarpeso.dart
+++ lib/pantallas/logicaboton/tomarpeso.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:intl/intl.dart';
 
 class TomarPeso extends StatelessWidget {
   const TomarPeso({super.key});
@@ -10,15 +11,33 @@ class TomarPeso extends StatelessWidget {
 
     DatabaseEvent event = await databaseReference.once();
     DataSnapshot snapshot = event.snapshot;
-    if (snapshot.value != null) {
 
+    print('Valor recuperado de Firebase: ${snapshot.value}'); // Agrega esta línea
+
+    if (snapshot.value != null) {
       double pesoGramos = double.tryParse(snapshot.value.toString()) ?? 0.0;
       double pesoKilogramos = pesoGramos / 1000;
-      return pesoKilogramos.toStringAsFixed(2);
+      String pesoKilogramosString = pesoKilogramos.toStringAsFixed(2);
+
+      // Obtener la fecha y hora actual
+      String fechaHora = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
+
+      // Registrar el peso en la sección Peso1 con un nombre descriptivo y la fecha
+      DatabaseReference peso1Ref = FirebaseDatabase.instance.ref().child('Usarios').child('Pesos');
+      await peso1Ref.push().set({
+        'nombre': 'peso',
+        'valor': pesoKilogramosString,
+        'fecha': fechaHora,
+      });
+
+      print('Datos registrados en Firebase: nombre=peso, valor=$pesoKilogramosString, fecha=$fechaHora'); // Agrega esta línea
+
+      return pesoKilogramosString;
     } else {
       return 'Peso no encontrado';
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
